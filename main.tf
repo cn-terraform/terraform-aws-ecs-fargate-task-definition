007675ad24b9dd7c75d76b916bb1da3cf1a3f30e
Julian Nonino <noninojulian@outlook.com>
2020-05-06 15:46:51 +0100

    Replacing comments

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-# ---------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # AWS ECS Task Execution Role
-# ---------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 resource "aws_iam_role" "ecs_task_execution_role" {
   name               = "${var.name_preffix}-ecs-task-execution-role"
   assume_role_policy = file("${path.module}/files/iam/ecs_task_execution_iam_role.json")
@@ -11,9 +11,9 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attach
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # ECS Task Definition
-# ---------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # Container Definition
 module "container_definition" {
   source  = "cloudposse/ecs-container-definition/aws"
