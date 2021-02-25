4ed6aa9de404325f78f1a28547c7ef29ab61158f
Julian Nonino <noninojulian@gmail.com>
2021-02-25 08:11:04 -0300

    Improving volumes variables

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_custom_policy
 # Container Definition
 module "container_definition" {
   source  = "cloudposse/ecs-container-definition/aws"
-  version = "0.45.2"
+  version = "0.50.0"
 
   container_name               = var.container_name
   container_image              = var.container_image
@@ -121,8 +121,17 @@ resource "aws_ecs_task_definition" "td" {
       dynamic "efs_volume_configuration" {
         for_each = lookup(volume.value, "efs_volume_configuration", [])
         content {
-          file_system_id = lookup(efs_volume_configuration.value, "file_system_id", null)
-          root_directory = lookup(efs_volume_configuration.value, "root_directory", null)
+          file_system_id          = lookup(efs_volume_configuration.value, "file_system_id", null)
+          root_directory          = lookup(efs_volume_configuration.value, "root_directory", null)
+          transit_encryption      = lookup(efs_volume_configuration.value, "transit_encryption", null)
+          transit_encryption_port = lookup(efs_volume_configuration.value, "transit_encryption_port", null)
+          dynamic "authorization_config" {
+            for_each = lookup(efs_volume_configuration.value, "authorization_config", [])
+            content {
+              access_point_id = lookup(authorization_config.value, "access_point_id", null)
+              iam             = lookup(authorization_config.value, "iam", null)
+            }
+          }
         }
       }
     }
