0fbb041c115f6d12a11956f2b8614826447b4a2e
ovcharenko <938082+ovcharenko@users.noreply.github.com>
2021-06-03 16:58:34 +0300

    [ADD] Optional tags (#14)

--- variables.tf
+++ variables.tf
@@ -5,6 +5,12 @@ variable "name_prefix" {
   description = "Name prefix for resources on AWS"
 }
 
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Resource tags"
+}
+
 #------------------------------------------------------------------------------
 # AWS ECS Container Definition Variables for Cloudposse module
 #------------------------------------------------------------------------------
