ff7d42884b0b616bee1a1e20eb3a7580af88c06e
Julian Nonino <noninojulian@gmail.com>
2021-06-11 08:53:55 +0100

    Fix build

--- examples/test/custom_iam_policy.tf
+++ examples/test/custom_iam_policy.tf
@@ -1,21 +1,21 @@
-ecs_task_execution_role_custom_policies = [
-  jsonencode(
-    {
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Effect" : "Allow",
-          "Action" : [
-            "secretsmanager:GetSecretValue"
-          ],
-          "Resource" : [
-            "arn:aws:secretsmanager:AWS_REGION:AWS_ACC:secret:SECRET_NAME"
-          ]
-        }
-      ]
-    }
-  )
-]
+# ecs_task_execution_role_custom_policies = [
+#   jsonencode(
+#     {
+#       "Version" : "2012-10-17",
+#       "Statement" : [
+#         {
+#           "Effect" : "Allow",
+#           "Action" : [
+#             "secretsmanager:GetSecretValue"
+#           ],
+#           "Resource" : [
+#             "arn:aws:secretsmanager:AWS_REGION:AWS_ACC:secret:SECRET_NAME"
+#           ]
+#         }
+#       ]
+#     }
+#   )
+# ]
 
 #This also works
 
