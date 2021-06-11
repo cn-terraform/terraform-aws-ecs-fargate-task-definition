a4a44ef27c358dc61bca55e16648c3090a0bc039
Julian Nonino <noninojulian@gmail.com>
2021-06-11 08:51:36 +0100

    Fix build

--- examples/test/custom_iam_policy.tf
+++ examples/test/custom_iam_policy.tf
@@ -1,24 +1,24 @@
 ecs_task_execution_role_custom_policies = [
-    jsonencode(
-      {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Action": [
-                    "secretsmanager:GetSecretValue"
-                ],
-                "Resource": [
-                    "arn:aws:secretsmanager:AWS_REGION:AWS_ACC:secret:SECRET_NAME"
-                ]
-            }
-        ]
-      }
-    )
-  ]
+  jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "secretsmanager:GetSecretValue"
+          ],
+          "Resource" : [
+            "arn:aws:secretsmanager:AWS_REGION:AWS_ACC:secret:SECRET_NAME"
+          ]
+        }
+      ]
+    }
+  )
+]
 
 #This also works
 
- ecs_task_execution_role_custom_policies = [
-   "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Action\":[\"secretsmanager:GetSecretValue\"],\"Resource\":[\"arn:aws:secretsmanager:AWS_REGION:AWS_ACC:secret:SECRET_NAME\"]}]}"
- ]
+# ecs_task_execution_role_custom_policies = [
+#   "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Action\":[\"secretsmanager:GetSecretValue\"],\"Resource\":[\"arn:aws:secretsmanager:AWS_REGION:AWS_ACC:secret:SECRET_NAME\"]}]}"
+# ]
